a29fc0189a331c7b42bd0b348996803b0f43c7e8
Agastus Joyson <agastus.j@sedai.io>
2025-08-04 14:32:00 +0530

    Initial Commit

---
+++ src/main/java/com/example/cloud_cost_estimator/CloudCostEstimatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud_cost_estimator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCostEstimatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCostEstimatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloud_cost_estimator/CloudCostEstimatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud_cost_estimator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCostEstimatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
